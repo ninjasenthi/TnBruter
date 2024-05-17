992dcbc61c62c12fbc076a2bf536d6a37ef35a0a
ninjasenthi <117979717+ninjasenthi@users.noreply.github.com>
2024-05-17 13:02:48 +0530

    Update Regno.py
    Using this addtional tool to generate a datasheet about student Regno

--- Regno.py
+++ Regno.py
@@ -1,19 +1,27 @@
 import json 
 
-def unfold_content(roll_no,no_back):
+def unfold_content(roll_no,argv):
     data=[]
-    for i in range(no_back):
-        roll= (roll_no - no_back) +i
-        data.append(roll)
+    if str(argv[0])=='-':
+        argv= int(argv[0:])
+        for i in range(-argv):
+            roll= (roll_no + i)
+            data.append(roll)
+    elif int(argv) >  0:
+        argv =int(argv)
+        for i in range(argv):
+            roll= (roll_no - argv) +i
+            data.append(roll)
     return data
-    
-Roll = input(" Enter roll no : ")
-Back = input(" Enter how many girl : ")
-content = unfold_content(int(Roll),int(Back))
+
+print(" Welcome to regno render :","\n")
+Roll = input(" Regno -> ")
+Arg = input("  move(+,-) -> ")
+content = unfold_content(int(Roll),Arg)
 print("\n",content)
 
 with open('Regsheet.txt','w') as file:
     for i in content:
         file.write(str(i)+'\n')
     print('')
-    print('complete data entered in datasheet ')
+    print('Completed : data entered in datasheet ')
